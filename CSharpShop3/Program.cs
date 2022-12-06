76a2941cb4cf98f7d76146dcb7be33eee03412c3
AnukaJapa <japaridze.ana96@gmail.com>
2022-12-06 13:09:23 +0100

    creato Exception per ph

--- CSharpShop3/Program.cs
+++ CSharpShop3/Program.cs
@@ -1,10 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using CSharpShop2;
-Acqua acqua1 = new Acqua(2, 50, 0.6, 10, "italia");
-CiboInScatola tonno = new CiboInScatola(3, 50, 200, "tonno");
 
-List<string> listaDellaFrutta = new List<string>()
+try
+{
+    Acqua acqua1 = new Acqua(2, 50, 0.6, 10, "italia");
+    CiboInScatola tonno = new CiboInScatola(3, 50, 200, "tonno");
+
+    List<string> listaDellaFrutta = new List<string>()
 {
     "Banana",
     "Mela",
@@ -18,13 +21,17 @@ List<string> listaDellaFrutta = new List<string>()
     "Pesca"
 };
 
-SacchettoDiFrutta sacchettoFrutta1 = new SacchettoDiFrutta(2, 50, listaDellaFrutta);
+    SacchettoDiFrutta sacchettoFrutta1 = new SacchettoDiFrutta(2, 50, listaDellaFrutta);
 
-//sacchettoFrutta1.StampaProdotto();
-acqua1.Bevi(1);
-acqua1.Riempi(2);
-acqua1.SetLitri(3);
+    //sacchettoFrutta1.StampaProdotto();
+    acqua1.Bevi(1);
+    acqua1.Riempi(2);
+    acqua1.SetLitri(3);
 
-//acqua1.StampaProdotto();
-tonno.StampaProdotto();
-Console.WriteLine();
+    //acqua1.StampaProdotto();
+    tonno.StampaProdotto();
+    Console.WriteLine();
+} catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}

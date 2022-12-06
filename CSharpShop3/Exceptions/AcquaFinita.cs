04eb0d2d660d88441ba6671c2fa0c62b6c5302a4
AnukaJapa <japaridze.ana96@gmail.com>
2022-12-06 13:11:14 +0100

    creato exception acquaFinita

---
+++ CSharpShop3/Exceptions/AcquaFinita.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpShop3.Exceptions
+{
+    internal class AcquaFinita
+    {
+    }
+}
